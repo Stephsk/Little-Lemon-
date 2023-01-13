91dd13e9e040964c7ef5ba3d9d21bb07d22080b6
Κουνάδη Στέφανη <kounadist@papadopoulos.local>
2023-01-13 11:15:54 +0200

    first webpage little lemon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <body>
+    <Header>
+      <logo></logo>
+<Nav>   </Nav>
+    </Header>
+    <Main>
+      <article>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Lorem...</p>
+        </article>
+        <article>
+        <h1>Specials</h1>
+        <h3>Greek Salad</h3>
+        <h3>Bruchetta</h3>
+        <h3>Lemon Desert</h3>
+        </article>
+        <article>
+        <h2>Testimonials</h2>
+        <h3>Rating</h3>
+        <h3>Rating</h3>
+        <h3>Rating</h3>
+        <h3>Rating</h3>
+        </article>
+        <article>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Amet...</p>
+        </article>
+        <Footer>
+
+        </Footer>
+
+    </Main>
+    <footer>
+
+    </footer>
+    </body>
   );
 }
 
