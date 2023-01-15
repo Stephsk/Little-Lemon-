d1748377212a0e19b1fd3f991684803d6af3d26f
Stephanie Kounadis <stephaniekou@yahoo.com>
2023-01-15 22:32:57 +0200

    setting up and breaking sections

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+function main(){
+    return(
+        <main>
+        <section>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>Lorem...</p>
+          <button>Reserve a Table</button>
+          <img></img>
+        </section>
+        <section>
+          <h1>Specials</h1>
+          <button>Online Menu</button>
+          <img></img>
+          <h3>Greek Salad</h3>
+          <img></img>
+          <h3>Bruchetta</h3>
+          <img></img>
+          <h3>Lemon Desert</h3>
+        </section>
+        <section>
+          <h2>Testimonials</h2>
+          <h3>Rating</h3>
+          <h3>Rating</h3>
+          <h3>Rating</h3>
+          <h3>Rating</h3>
+        </section>
+        <section>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>Amet...</p>
+          <img></img>
+          <img></img>
+        </section>
+      </main>
+    )
+}
